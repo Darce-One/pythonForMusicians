844ce9dc8b6b49df2de7398049f6b8a4789a657d
Andreas Papaeracleous <andreas@Andreas-Beast.local>
2024-04-25 00:04:11 +0300

    added lesson 2 materials

---
+++ lesson2/assignment_1.py
@@ -0,0 +1,42 @@
+"""
+This assignment was given at the end of lesson 2.
+
+Learning Outcomes of the assessment:
+    - Understand creating arrays
+    - Ability to recognise what to try
+    - Ability to read the documentation when "hunting"
+
+
+Assessment Brief:
+    Create plot of 10 uniformly random points in range [-1:1]
+    Give the plot a title, and label the axes appropriately.
+    You may start from this template:
+"""
+
+# Importing libraries
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    # Your code goes here. Replace the 'None' statements with what you need
+
+    # Creating the x-axis with 10 points:
+    x_array = None
+
+    # Creating 10 uniformly random points in range [-1:1]
+    y_random = None
+
+    # Plotting code:
+    plt.plot(None)
+
+    # Adding a Title, xlabel and ylabel
+    None
+    None
+    None
+
+    # Show the plot
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
